ece2324e47e80eb48f15f27687b9650ae1dfc955
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2024-05-17 14:12:43 +0100

    qwyiy

---
+++ BCTAcademy/JS/index.js
@@ -0,0 +1,58 @@
+// console.log("I am a winner.")
+// console.log("Hello World!");
+
+// nameY = "Mariam" //String
+// let age = 22 //number integer\
+// console.log(age)
+// const Nigerian = true//boolean
+// age = 20
+// console.log(age)
+// console.log(nameY)
+// y="20"
+
+// console.log(y !== age)
+// sentence = nameY + " is " + age + " years old."
+
+// console.log(sentence)
+num1 = 12;
+num2 = 6;
+
+function addition() {
+  return 2 + 4;
+}
+function subtraction() {
+  return 2 - 4;
+}
+function multiplication() {
+  return 2 * 4;
+}
+function division() {
+  return 2 / 4;
+}
+a = addition();
+b = subtraction();
+c = multiplication();
+d = division();
+
+// console.log(a)
+// console.log(b)
+// console.log(c)
+// console.log(d)
+
+//enter first number
+//enter second number
+//add
+//minus
+//answer
+
+const car = {
+  id: 1,
+  name: "G-Wagon",
+  color: "Black",
+  weight: 2500,
+  price: "$2M",
+  petrol: true,
+  electric: false,
+};
+// console.log(car.electric);
+//Arrays of Objects.
