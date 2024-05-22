20892e41d190f2ba7dd880ee20fa17007773f079
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2024-05-22 14:36:51 +0100

    Update index.js

--- BCTAcademy/JS/index.js
+++ BCTAcademy/JS/index.js
@@ -14,25 +14,25 @@
 // sentence = nameY + " is " + age + " years old."
 
 // console.log(sentence)
-num1 = 12;
-num2 = 6;
+// num1 = 12;
+// num2 = 6;
 
-function addition() {
-  return 2 + 4;
-}
-function subtraction() {
-  return 2 - 4;
-}
-function multiplication() {
-  return 2 * 4;
-}
-function division() {
-  return 2 / 4;
-}
-a = addition();
-b = subtraction();
-c = multiplication();
-d = division();
+// function addition() {
+//   return 2 + 4;
+// }
+// function subtraction() {
+//   return 2 - 4;
+// }
+// function multiplication() {
+//   return 2 * 4;
+// }
+// function division() {
+//   return 2 / 4;
+// }
+// a = addition();
+// b = subtraction();
+// c = multiplication();
+// d = division();
 
 // console.log(a)
 // console.log(b)
@@ -45,14 +45,70 @@ d = division();
 //minus
 //answer
 
-const car = {
-  id: 1,
-  name: "G-Wagon",
-  color: "Black",
-  weight: 2500,
-  price: "$2M",
-  petrol: true,
-  electric: false,
-};
+// const car = {
+//   id: 1,
+//   name: "G-Wagon",
+//   color: "Black",
+//   weight: 2500,
+//   price: "$2M",
+//   petrol: true,
+//   electric: true,
+// };
 // console.log(car.electric);
 //Arrays of Objects.
+// const cars = [
+//   {
+//     id: 1,
+//     name: "Brabus G-Wagon",
+//     color: "Black",
+//     weight: 2500,
+//     price: "$5M",
+//     petrol: false,
+//     electric: true,
+//   },
+//   {
+//     id: 2,
+//     name: "G-Wagon",
+//     color: "Black",
+//     weight: 2500,
+//     price: "$2M",
+//     petrol: true,
+//     electric: false,
+//   },
+//   {
+//     id: 3,
+//     name: "Bugatti",
+//     color: "White",
+//     weight: 2700,
+//     price: "$4M",
+//     petrol: true,
+//     electric: false,
+//   },
+//   {
+//     id: 4,
+//     name: "BMW",
+//     color: "Blue",
+//     weight: 2200,
+//     price: "$1.5M",
+//     petrol: true,
+//     electric: false,
+//   },
+//   {
+//     id: 5,
+//     name: "Supra",
+//     color: "Yellow",
+//     weight: 2500,
+//     price: "$1.2M",
+//     petrol: true,
+//     electric: false,
+//   },
+// ];
+
+// console.log(cars.length);
+
+// const filter = cars.filter((car) => (car.id == 4))
+// console.log(filter);
+
+cars.map((car) => (
+  console.log(car.id + " " + car.name + " " + car.price)
+))
