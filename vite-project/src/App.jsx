8de1a092837ef9574682cf7f1a49c97bffb35c6d
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2024-06-14 10:59:42 +0100

    Update App.jsx

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import Navbar from './components/Navbar'
-import Hero from './components/Hero'
+import Footer from './components/Footer'
 
 export default function App() {
   return (
     <div>
       <Navbar />
-      <Hero />
+      <Footer />
     </div>
   )
 }
